9037094088a9b6ebe68c76cdb1b4eac41749d551
jxaces <jadeaceflor@gmail.com>
2024-12-17 09:23:51 +0800

    final commit

--- src/Core/View.php
+++ src/Core/View.php
@@ -23,21 +23,24 @@ class View
         $page_js = $page_js ?? '';
         $title = $title ?? 'Job Portal';
 
-        // Set sidebar visibility: hide sidebar for login.html
-        $sidebar = ($template !== 'login.html') ? true : false;
+        // Set visibility flags based on the template
+        $show_nav = ($template !== 'login.html');
+        $show_footer = ($template !== 'login.html');
 
         if (file_exists($layoutPath)) {
             ob_start();
             include $layoutPath;
             $layoutContent = ob_get_clean();
 
+            // Replace placeholders
             $layoutContent = str_replace('{{ page_css }}', $page_css, $layoutContent);
             $layoutContent = str_replace('{{ content }}', $content, $layoutContent);
             $layoutContent = str_replace('{{ page_js }}', $page_js, $layoutContent);
             $layoutContent = str_replace('{{ title }}', htmlspecialchars($title), $layoutContent);
 
-            // Pass the sidebar variable to the layout
-            $layoutContent = str_replace('{{ sidebar }}', $sidebar, $layoutContent);
+            // Replace visibility flags with PHP logic
+            $layoutContent = str_replace('{{ show_nav }}', $show_nav, $layoutContent);
+            $layoutContent = str_replace('{{ show_footer }}', $show_footer, $layoutContent);
 
             echo $layoutContent;
         } else {
